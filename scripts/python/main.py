ef02e14f47e980902e3549a7defb05c0d8efb0f4
Alexey Garifov <al.garifov@gmail.com>
2024-07-03 11:30:24 +0300

    major bug discribed

--- scripts/python/main.py
+++ scripts/python/main.py
@@ -1,18 +1,26 @@
 import sys
 import os
 
-
 """NB! This trick needed for 3d party libraries to work."""
 path = os.path.join(os.path.dirname(__file__), "site-packages")
 sys.path.insert(0, path)
 
 from breakdown.logic import get_prepared_dialog
 
+
 # FIXME: known bug: sometimes item in table can dissapear when swapping one wrong version to another
 #        UPD: when /obj/testSphere/testSphere/file is version 5 and /obj/testBox/testBox/file too
 #        hou.fileReferences stops returning /obj/testBox/testBox/file
 #        have no idea why. Houdini FX 19.5.368 Py3.9 Windows 10 02/Jule/2024
-
+#
+# FIXME:    MAJOR HOUDINI BUG!
+#       hou.fileReferences() will omit all of parms containing the same UNEXPANDED value except one!
+#       for example:
+#       hou.parm("/obj/geo1/file1/file") contains path $JOB/$OS.bgeo.sc
+#       AND
+#       hou.parm("/obj/geo2/file2/file") contains path $JOB/$OS.bgeo.sc
+#       ONLY ONE of them will be returned with no respect these are different paths after hou.expandString()
+#       This is one more reason not to use $OS in production.
 
 def main():
     dialog = get_prepared_dialog()
